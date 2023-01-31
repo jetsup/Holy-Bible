70ca488104fc78df103c2f47d69e8fe44798d697
George Ngigi <ngigegeorge023@gmail.com>
2023-01-31 18:04:28 +0300

    Added chapter selection and navigation. Modified the Bible file to improve the overall performance.

--- app/src/main/java/com/jetsup/holybible/ReadActivity.java
+++ app/src/main/java/com/jetsup/holybible/ReadActivity.java
@@ -1,67 +1,40 @@
 package com.jetsup.holybible;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.jetsup.holybible.adapters.VerseRecyclerViewAdapter;
+
 import java.util.Objects;
 
 public class ReadActivity extends AppCompatActivity {
-    final String TAG = "MyTag_RA";
-    String bookTitle, alias, nextAlias;
-    String line, chapterString;
-    int bookIndex;
+    String bookTitle;
+    String bookFile;
+    VerseRecyclerViewAdapter verseAdapter;
+    int bookIndex, readChapter;
+    RecyclerView verseRecyclerView;
 
-    String[] aliases = {"Ge", "Exo", "Lev", "Num", "Deu", "Josh", "Jdgs", "Ruth", "1Sm", "2Sm", "1Ki", "2Ki", "1Chr", "2Chr", "Ezra", "Neh", "Est", "Job", "Psa", "Prv", "Eccl", "SSol", "Isa", "Jer", "Lam", "Eze", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nahum", "Hab", "Zep", "Hag", "Zec", "Mal", "Mat", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phi", "Col", "1Th", "2Th", "1Tim", "2Tim", "Titus", "Phmn", "Heb", "Jas", "1Pet", "2Pet", "1Jn", "2Jn", "3Jn", "Jude", "Rev"};
+    String[] fileAliases;// = {"Ge", "Exo", "Lev", "Num", "Deu", "Josh", "Jdgs", "Ruth", "1Sm", "2Sm", "1Ki", "2Ki", "1Chr", "2Chr", "Ezra", "Neh", "Est", "Job", "Psa", "Prv", "Eccl", "SSol", "Isa", "Jer", "Lam", "Eze", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nahum", "Hab", "Zep", "Hag", "Zec", "Mal", "Mat", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phi", "Col", "1Th", "2Th", "1Tim", "2Tim", "Titus", "Phmn", "Heb", "Jas", "1Pet", "2Pet", "1Jn", "2Jn", "3Jn", "Jude", "Rev"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
+        verseRecyclerView = findViewById(R.id.verseRecyclerView);
+        verseRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        fileAliases = getResources().getStringArray(R.array.filename_alias);
+
         bookTitle = getIntent().getExtras().getString("BookTitle");
+        readChapter = getIntent().getExtras().getInt("Chapter");
         bookIndex = getIntent().getExtras().getInt("BookIndex");
-        alias = aliases[bookIndex];
-        if (bookIndex < 65) {
-            nextAlias = aliases[bookIndex + 1];
-        }
+        Objects.requireNonNull(getSupportActionBar()).setTitle(bookTitle + " " + readChapter);
 
-        Objects.requireNonNull(getSupportActionBar()).setTitle(bookTitle);
-//        Toast.makeText(this, "Book: '" + bookTitle + "' Book index: " + bookIndex + " Alias: '" + alias + "'", Toast.LENGTH_SHORT).show();
-        try {
-            readChapter();
-        } catch (IOException e) {
-            Log.e(TAG, "Error: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void readChapter() throws IOException {
-        InputStream theBible = getResources().openRawResource(R.raw.bible_kjv);
-        Log.w(TAG, "readChapter: Can read: " + theBible.available());
-
-        while (true) {
-            if (theBible.available() > 0) {
-                line += String.valueOf((char) theBible.read());
-                if (line.endsWith("\n")) {
-                    if (line.startsWith(nextAlias)) {
-                        break;
-                    } else {
-                        if (line.startsWith(alias)) {
-                            chapterString += line;
-                        } else {
-                            line = "";
-                        }
-                    }
-                }
-            } else {
-                Log.w(TAG, "readChapter: " + chapterString);
-                break;
-            }
-        }
-
-        Log.i(TAG, "readChapter: " + line);
+        bookFile = fileAliases[bookIndex] + readChapter;
+        verseAdapter = new VerseRecyclerViewAdapter(this, bookFile);
+        verseRecyclerView.setAdapter(verseAdapter);
     }
 }
--- app/src/main/java/com/jetsup/holybible/adapters/BookTitleRecyclerViewAdapter.java
+++ app/src/main/java/com/jetsup/holybible/adapters/BookTitleRecyclerViewAdapter.java
@@ -6,15 +6,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.divider.MaterialDivider;
 import com.jetsup.holybible.R;
-import com.jetsup.holybible.ReadActivity;
+import com.jetsup.holybible.SelectChapter;
 
 import java.util.Arrays;
 import java.util.List;
@@ -46,14 +45,11 @@ public class BookTitleRecyclerViewAdapter extends RecyclerView.Adapter<BookTitle
     public void onBindViewHolder(@NonNull MyBookTitleVH holder, int position) {
         holder.bookTitle.setText(bookTitles.get(position));
         holder.bookDescription.setText(bookDescription.get(position));
-        if (position == bookTitles.size() - 1) {
-            holder.bookDivider.setVisibility(View.GONE);
-        }
         holder.bookTitleTile.setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.putExtra("BookTitle", bookTitles.get(position));
             intent.putExtra("BookIndex", position);
-            intent.setClass(context, ReadActivity.class);
+            intent.setClass(context, SelectChapter.class);
             context.startActivity(intent);
         });
     }
@@ -66,15 +62,14 @@ public class BookTitleRecyclerViewAdapter extends RecyclerView.Adapter<BookTitle
     static class MyBookTitleVH extends RecyclerView.ViewHolder {
         CircleImageView smallLogo;
         TextView bookTitle, bookDescription;
-        MaterialDivider bookDivider;
-        RelativeLayout bookTitleTile;
+        CardView bookTitleTile;
 
         public MyBookTitleVH(@NonNull View itemView) {
             super(itemView);
             smallLogo = itemView.findViewById(R.id.smallLogo);
             bookTitle = itemView.findViewById(R.id.bookTitle);
             bookDescription = itemView.findViewById(R.id.bookDescription);
-            bookDivider = itemView.findViewById(R.id.bookDivider);
+//            bookDivider = itemView.findViewById(R.id.bookDivider);
             bookTitleTile = itemView.findViewById(R.id.bookTitleTile);
         }
     }
